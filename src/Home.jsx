087b0a2d524f83fe1278a0387acc64103b636f2f
Favoured08 <feranmiadeosun1@gmail.com>
2024-04-08 22:09:40 +0100

    modified

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+    return ( 
+        <div className="container">
+            <div className="text">
+                <h1>I am <span>Oluwaferanmi Adeosun Dorcas</span>,Web Developer</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea similique, modi nesciunt minus maiores odit, quos sequi velit eveniet atque, quae dolorem illo?Lorem ipsum dolor sit.
+                </p>
+                <button type="button" className="About-btn">About</button>
+                <button type="button" className="download-btn">Download Cv</button>
+            </div>
+            <div className="image">
+                <img src="" alt="profile picture" />
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
